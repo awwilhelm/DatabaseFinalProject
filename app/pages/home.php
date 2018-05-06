681a3a1d73f6a860dad7d0292026ae4255888102
Alex Wilhelm <awwilhelm2014@gmail.com>
2018-05-06 17:20:14 -0500

    connection test

--- app/pages/home.php
+++ app/pages/home.php
@@ -17,6 +17,10 @@
                 } else {
                     echo 'Connection status bad';
                 }
+                $result = pg_query($dbconn, "SELECT P.portfolio_id, C.company_name, P.date_created, P.updated_on FROM s18group02.Portfolio P JOIN s18group02.Client C ON P.client_id = C.client_id ORDER BY updated_on DESC;");
+                console.log(var_dump(pg_fetch_all($result)));
+                console.log($result);
+                echo 'here';
             ?>
         </div>
     </div>
